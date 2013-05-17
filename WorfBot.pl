6a5f852bcabc99eee64fad92b6f255841a56a656
Michael Stark <stark3@gmail.com>
2013-05-17 14:27:38 -0400

    Small tweaks and fixes.

--- WorfBot.pl
+++ WorfBot.pl
@@ -5,11 +5,23 @@ use Net::IRC;
 use IO::File;
 use Date::Format;
 use JSON;
+use Data::Dumper;
 use strict;
 use warnings;
 
-my %honorable_phrases;
+my %honorablePhrases;
 my $biblesOrgAPIKey;
+my %versionInfo;
+
+sub load_versions {
+  my $versionFH;
+  open($versionFH, "<versions.json");
+  my $versionString = <$versionFH>;
+  close($versionFH);
+  my $hashref = decode_json($versionString);
+  %versionInfo = %$hashref;
+  print Dumper(\%versionInfo);
+}
 
 sub debug_print {
   my $message = shift;
@@ -20,19 +32,25 @@ sub debug_print {
   close($ofh);
 }
 
+sub give_help {
+  my ($conn, $target) = @_;
+  $conn->privmsg($target, "You may ask me about which things have honor by typing '!honor <phrase>'\n");
+}
+
 sub check_honor {
   my ($conn, $phrase, $target) = @_;
   debug_print("'$target' asked about '$phrase'");
   my $response;
-  if (exists $honorable_phrases{lc $phrase}) {
+  my $lcPhrase = lc($phrase);
+  if (exists $honorablePhrases{$lcPhrase}) {
     debug_print("Found '$phrase' in hash, using predetermined answer");
-    if ($honorable_phrases{lc $phrase} eq "Honorable") {
+    if ($honorablePhrases{$lcPhrase} eq "Honorable") {
       $response = "$phrase has honor.";
     } else {
        $response = "$phrase is without honor.";
     }
   } else {
-    my $md5hash = md5_hex(lc $phrase);
+    my $md5hash = md5_hex($lcPhrase);
     debug_print "Using hash '$md5hash' for phrase '$phrase'";
     if ($md5hash =~ /[0-7]$/) {
       $response = "$phrase has honor.";
@@ -57,7 +75,7 @@ sub quote_bible {
 
 sub add_word {
   my ($word, $honor) = @_;
-  $honorable_phrases{lc $word} = $honor;
+  $honorablePhrases{lc $word} = $honor;
 }
 
 sub join_channel {
@@ -66,10 +84,16 @@ sub join_channel {
   $conn->privmsg($channel, 'I am WorfBot, son of MoghBot');
 }
 
+sub part_channel {
+  my ($conn, $channel) = @_;
+  $conn->privmsg($channel, "Today is a good day to die!");
+  $conn->part($channel);
+}
+
 sub load_words {
   my $ifh;
   open($ifh, "<honorable_phrases.txt") or debug_print("Error - could not open honorable_phrases.txt for read ($!)");
-  %honorable_phrases = ();
+  %honorablePhrases = ();
   while (<$ifh>) {
     my $line = $_;
     chomp($line);
@@ -84,8 +108,8 @@ sub load_words {
 
 sub save_words {
   my @lines;
-  foreach my $phrase (keys %honorable_phrases) {
-    push(@lines, "$phrase:$honorable_phrases{$phrase}");
+  foreach my $phrase (keys %honorablePhrases) {
+    push(@lines, "$phrase:$honorablePhrases{$phrase}");
   }
   my $ofh;
   open($ofh, ">honorable_phrases.txt") or debug_print("Could not open honorable_phrases.txt for write ($!)");
@@ -112,6 +136,8 @@ sub on_public {
 
   if ($text =~ /^\!honor (.+)/i) {
     check_honor($conn, $1, $event->{to}[0]);
+  } elsif ($text =! /^\!help/i) {
+    give_help($conn, $event->{nick});
   }
 }
 
@@ -139,15 +165,14 @@ sub on_msg {
       add_word($word, "Dishonorable");
     }
   } elsif ($text =~ /^help/i) {
-    $conn->privmsg($nick, "You may ask me about which things have honor by typing '!honor <phrase>'\n");
+    give_help($conn, $nick);
   } elsif ($text =~ /^join (#.+)$/i && $nick eq "mstark") {
     my $channel = $1;
     join_channel($conn, $channel);
   } elsif ($text =~ /^part (#.+)$/i && $nick eq "mstark") {
     my $channel = $1;
-    $conn->privmsg($channel, "Today is a good day to die!");
-    $conn->part($channel);
-  } elsif ($text =~ /^\!honor (.+)/i) {
+    part_channel($conn, $channel);
+  } elsif ($text =~ /^honor (.+)/i) {
     check_honor($conn, $1, $nick);    
   }
 }
@@ -162,6 +187,9 @@ END
 
 # 'Main' function
 # Args - server_address [channel1 channel2 channel3 ...]
+
+# Load Bible versions in THE FUTURE...
+#load_versions();
 
 if (scalar(@ARGV) < 1) {
   print get_usage_string();
